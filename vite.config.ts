88ae5a36e141a7b310b1d81c04fc002219eda6fe
matildasoderhall <soderhall.matilda@gmail.com>
2026-09-16 12:23:23 +0200

    feat: remove unused assets and styles, add LCH color utilities and media query mixins
    - Deleted unused image assets: hero.png, react.svg, and vite.svg.
    - Removed index.css and replaced it with a new SCSS structure.
    - Introduced new mixins for LCH color utilities and media query helpers.
    - Added accessibility styles for screen readers and focus indicators.
    - Created a base reset and base styles for consistent styling across the application.
    - Established a token system for colors, typography, and viewport settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+      '@styles': path.resolve(import.meta.dirname, './src/styles'),
+      '@components': path.resolve(import.meta.dirname, './src/components'),
+      '@layouts': path.resolve(import.meta.dirname, './src/layouts'),
+      '@assets': path.resolve(import.meta.dirname, './src/assets'),
+      '@models': path.resolve(import.meta.dirname, './src/models'),
+      '@pages': path.resolve(import.meta.dirname, './src/pages'),
+      '@hooks': path.resolve(import.meta.dirname, './src/hooks'),
+      '@services': path.resolve(import.meta.dirname, './src/services'),
+    },
+  },
 })
