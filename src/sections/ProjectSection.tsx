6c39b9c2a87931e0715825ab30b2fd66793580b5
matildasoderhall <soderhall.matilda@gmail.com>
2026-09-21 09:40:11 +0200

    ✨ feat: add ProjectSection component with styles and structure

---
+++ src/sections/ProjectSection.tsx
@@ -0,0 +1,9 @@
+import './ProjectSection.scss';
+
+export const ProjectSection = () => {
+  return (
+    <div className="project-section">
+      <h2 className='project-section__title'>Selected projects</h2>
+    </div>
+  );
+};
