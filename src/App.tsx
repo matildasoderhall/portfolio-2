2918b805aa38fd6709d5a904b9261864e61e7d5e
matildasoderhall <soderhall.matilda@gmail.com>
2026-09-24 14:23:34 +0200

    ✨ feat: include ContactSection in App for user interaction

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import '@styles/globals.scss';
 import { HeroSection  } from './sections/HeroSection';
 import { ProjectSection } from './sections/ProjectSection';
 import { AboutSection } from './sections/AboutSection';
+import { ContactSection } from './sections/ContactSection';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <HeroSection />
       <ProjectSection />
       <AboutSection />
+      <ContactSection />
     </>
   );
 }
